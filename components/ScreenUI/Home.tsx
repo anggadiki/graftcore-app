7388e318d49d4179e88fb32ec98c92008129b3e6
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2024-03-15 13:45:39 +0700

    refacto style in home screen ui

--- components/ScreenUI/Home.tsx
+++ components/ScreenUI/Home.tsx
@@ -1,35 +1,22 @@
-import { Image } from "react-native";
-import { Text, View } from "../Themed";
+import { Image, StyleSheet } from "react-native";
+import { View } from "../Themed";
 import Button from "../Atoms/Buttton";
 import { Link } from "expo-router";
 
 const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
+    <View style={styles.container}>
       <Image
         source={require("../../assets/images/logo.png")}
-        style={{ marginTop: 200, width: 172, height: 37 }}
+        style={styles.logo}
         alt="logo"
       />
       <Image
         source={require("../../assets/images/gambar.png")}
+        style={styles.image}
         alt="home"
-        style={{ marginTop: 72, marginBottom: 63, width: 241, height: 195 }}
       />
-      <View
-        style={{
-          width: "100%",
-          backgroundColor: "white",
-          gap: 24,
-          paddingHorizontal: 30,
-        }}
-      >
+      <View style={styles.buttonContainer}>
         <Button>
           <Link href="/auth/(log in)/logIn">Log In</Link>
         </Button>
@@ -40,5 +27,30 @@ const HomeScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: {
+    marginTop: 200,
+    width: 172,
+    height: 37,
+  },
+  image: {
+    marginTop: 72,
+    marginBottom: 63,
+    width: 241,
+    height: 195,
+  },
+  buttonContainer: {
+    width: "100%",
+    backgroundColor: "white",
+    paddingHorizontal: 30,
+    gap: 24,
+  },
+});
 
 export default HomeScreen;
