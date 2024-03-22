03b0d6b91c39bfbe7f170099e147dc053c45ee71
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2024-03-22 18:59:28 +0700

    refacto clean code style in login screen

--- components/ScreenUI/LogIn.tsx
+++ components/ScreenUI/LogIn.tsx
@@ -1,11 +1,12 @@
+import React from "react";
+import { Image, StyleSheet, TextInput } from "react-native";
+import { Text, View } from "../Themed";
 import {
   useFonts,
   NotoSans_400Regular,
   NotoSans_600SemiBold,
   NotoSans_800ExtraBold,
 } from "@expo-google-fonts/noto-sans";
-import { Text, View } from "../Themed";
-import { Image, TextInput } from "react-native";
 import Button from "../Atoms/Buttton";
 import { Link, router } from "expo-router";
 
@@ -15,120 +16,119 @@ const LogInScreeen = () => {
     NotoSans_600SemiBold,
     NotoSans_800ExtraBold,
   });
+
   if (!fontsLoaded && !fontError) {
     return null;
   }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "white",
-        margin: 24,
-      }}
-    >
+    <View style={styles.container}>
       <Image
         source={require("../../assets/images/logo.png")}
-        style={{ width: 172, height: 37, marginTop: 70 }}
+        style={styles.logo}
       />
-      <Text
-        style={{
-          fontFamily: "NotoSans_600SemiBold",
-          color: "black",
-          marginTop: 16,
-          fontSize: 24,
-        }}
-      >
-        Hello there!
-      </Text>
-      <View
-        style={{
-          width: "100%",
-          backgroundColor: "white",
-          marginTop: 32,
-          gap: 16,
-        }}
-      >
-        <View style={{ width: "100%", backgroundColor: "white" }}>
-          <Text
-            style={{
-              fontFamily: "NotoSans_400Regular",
-              color: "black",
-              fontWeight: "400",
-              fontSize: 16,
-            }}
-          >
-            Email Address
-          </Text>
+      <Text style={styles.heading}>Hello there!</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputLabel}>Email Address</Text>
           <TextInput
             placeholder="youremail@domain.com"
             textContentType="emailAddress"
-            style={{
-              backgroundColor: "#ECECEC80",
-              paddingVertical: 13,
-              borderRadius: 8,
-              paddingLeft: 16,
-              borderWidth: 1,
-              borderColor: "#B0ABAD",
-              fontSize: 16,
-              color: "black",
-            }}
+            style={styles.input}
           />
         </View>
-        <View style={{ width: "100%", backgroundColor: "white" }}>
-          <Text
-            style={{
-              fontFamily: "NotoSans_400Regular",
-              color: "black",
-              fontWeight: "400",
-              fontSize: 16,
-            }}
-          >
-            Password
-          </Text>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputLabel}>Password</Text>
           <TextInput
             placeholder="**********"
             textContentType="password"
             secureTextEntry={true}
-            style={{
-              backgroundColor: "#ECECEC80",
-              paddingVertical: 13,
-              borderRadius: 8,
-              paddingLeft: 16,
-              borderWidth: 1,
-              borderColor: "#B0ABAD",
-              fontSize: 16,
-              color: "black",
-            }}
+            style={styles.input}
           />
         </View>
       </View>
-      <View style={{ width: "100%", backgroundColor: "white", marginTop: 28 }}>
+      <View style={styles.buttonContainer}>
         <Button
           title="Continue"
           onPress={() => router.push("/auth/(passcode)/createPasscode")}
-        ></Button>
+        />
       </View>
       <Link
         href="/auth/(reset password)/forgotPassword"
-        style={{ marginTop: 24 }}
+        style={styles.forgotPasswordLink}
       >
-        <Text
-          style={{
-            color: "#270162",
-            fontFamily: "NotoSans_600SemiBold",
-            fontSize: 16,
-          }}
-        >
-          Change Password
-        </Text>
+        <Text style={styles.forgotPasswordText}>Change Password</Text>
       </Link>
       <Image
         source={require("../../assets/images/fingerprint.png")}
-        style={{ marginTop: 28, width: 160, height: 160 }}
-      ></Image>
+        style={styles.fingerprintImage}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+    margin: 24,
+  },
+  logo: {
+    width: 172,
+    height: 37,
+    marginTop: 70,
+  },
+  heading: {
+    fontFamily: "NotoSans_600SemiBold",
+    color: "black",
+    marginTop: 16,
+    fontSize: 24,
+  },
+  inputContainer: {
+    width: "100%",
+    backgroundColor: "white",
+    marginTop: 32,
+    gap: 16,
+  },
+  inputWrapper: {
+    width: "100%",
+    backgroundColor: "white",
+  },
+  inputLabel: {
+    fontFamily: "NotoSans_400Regular",
+    color: "black",
+    fontWeight: "400",
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: "#ECECEC80",
+    paddingVertical: 13,
+    borderRadius: 8,
+    paddingLeft: 16,
+    borderWidth: 1,
+    borderColor: "#B0ABAD",
+    fontSize: 16,
+    color: "black",
+  },
+  buttonContainer: {
+    width: "100%",
+    backgroundColor: "white",
+    marginTop: 28,
+  },
+  forgotPasswordLink: {
+    marginTop: 24,
+  },
+  forgotPasswordText: {
+    color: "#270162",
+    fontFamily: "NotoSans_600SemiBold",
+    fontSize: 16,
+  },
+  fingerprintImage: {
+    marginTop: 28,
+    width: 160,
+    height: 160,
+  },
+});
 
 export default LogInScreeen;
