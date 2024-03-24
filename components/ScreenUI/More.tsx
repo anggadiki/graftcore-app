78b7514d4f11f698b9847dc5c9842875e3bf0cc0
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2024-03-24 16:36:17 +0700

    refacto clean code style in more screen

--- components/ScreenUI/More.tsx
+++ components/ScreenUI/More.tsx
@@ -1,20 +1,24 @@
+import React from "react";
+import { StyleSheet } from "react-native";
 import { router } from "expo-router";
 import Button from "../Atoms/Buttton";
-import { Text, View } from "../Themed";
+import { View } from "../Themed";
 
 const MoreScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
+    <View style={styles.container}>
       <Button title="Log Out" onPress={() => router.push("/")} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+});
 
 export default MoreScreen;
